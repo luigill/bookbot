9f513fd043366ed8fb49f92f7415417be7ab112b
luigill <lgaquini@proton.me>
2025-04-02 17:09:06 -0300

    Finalizando primeira versao

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words 
-
+from stats import count_chars 
+from stats import dict_to_sorted_list
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,8 +9,18 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book = get_book_text("./books/frankenstein.txt")
-    print(count_words(book))
+    file_path = "./books/frankenstein.txt"
+    book = get_book_text(file_path)
+    result_dict = count_chars(book)
+    sorted_chars = dict_to_sorted_list(result_dict)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(book)} total words")
+    print("--------- Character Count -------")
+    for char_entry in sorted_chars:
+        print(f"{char_entry["char"]}: {char_entry["count"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,41 @@
 def count_words(string):
     word_list = string.split()
 
-    return f"{len(word_list)} words found in the document"
+    return len(word_list)
+
+
+def count_chars(string):
+
+    result_count = {}
+
+    lower_string = string.lower()
+
+    for letter in lower_string:
+        if letter not in result_count:
+            result_count[letter] = 0
+            result_count[letter] += 1
+        else:
+            result_count[letter] += 1
+
+    return result_count
+
+def sort_on(dict):
+    return dict["count"]
+
+def dict_to_sorted_list(dict):
+
+    char_list = []
+
+    for char, count in dict.items():
+        char_list.append({"char": char, "count":count})
+
+    char_list.sort(reverse=True, key=sort_on)
+
+    return char_list
+
+
+
+
+
+
+
