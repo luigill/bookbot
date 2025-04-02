8450e14415b2ee594eddbd9caa4c7c553ba94191
luigill <lgaquini@proton.me>
2025-04-02 17:15:36 -0300

    Finalized Project

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from stats import count_words
 from stats import count_chars 
 from stats import dict_to_sorted_list
 
+import sys
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -9,18 +11,23 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    file_path = "./books/frankenstein.txt"
-    book = get_book_text(file_path)
-    result_dict = count_chars(book)
-    sorted_chars = dict_to_sorted_list(result_dict)
 
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {file_path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {count_words(book)} total words")
-    print("--------- Character Count -------")
-    for char_entry in sorted_chars:
-        print(f"{char_entry["char"]}: {char_entry["count"]}")
-    print("============= END ===============")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        file_path = sys.argv[1]
+        book = get_book_text(file_path)
+        result_dict = count_chars(book)
+        sorted_chars = dict_to_sorted_list(result_dict)
+
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {file_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {count_words(book)} total words")
+        print("--------- Character Count -------")
+        for char_entry in sorted_chars:
+            print(f"{char_entry["char"]}: {char_entry["count"]}")
+        print("============= END ===============")
 
 main()
