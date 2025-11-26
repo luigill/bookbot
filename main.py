b664a4387621459f38e272ff742271f29a0fd513
luigill <lgaquini@gmail.com>
2025-11-26 16:15:14 -0300

    report done

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from stats import get_num_words
+from stats import get_num_chars, get_num_words, print_chars_count, sort_chars
 
 path_book = "./books/frankenstein.txt"
 
 
 def get_book_text(book_path):
     book_content = ""
-    with open(book_path) as f:
+    with open(book_path, encoding="utf-8-sig") as f:
         book_content = f.read()
     return book_content
 
 
 def main():
     content = get_book_text(path_book)
+    print(get_num_chars(content))
+    # print(sort_chars(get_num_chars(content)))
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_book}...")
+    print("----------- Word Count ----------")
     print(get_num_words(content))
+    print("--------- Character Count -------")
+    print_chars_count(sort_chars(get_num_chars(content)))
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,38 @@
 def get_num_words(content: str):
     splitted = content.split()
     return f"Found {len(splitted)} total words."
+
+
+def get_num_chars(content: str):
+    char_cout = {}
+
+    for i in content:
+        processing_char = i.lower()
+        # verify if the key that is being processed already exists
+        if processing_char not in char_cout.keys():
+            char_cout[processing_char] = 1
+        else:
+            char_cout[processing_char] += 1
+
+    return char_cout
+
+
+# Auxiliar function of sorting
+def sort_on(dictionary: dict):
+    return dictionary["num"]
+
+
+def sort_chars(dictionary: dict):
+    list_dicts = []
+    for key, value in dictionary.items():
+        list_dicts.append(dict(char=key, num=value))
+    list_dicts.sort(key=sort_on, reverse=True)
+    return list_dicts
+
+
+def print_chars_count(list_of_dict: list):
+    for i in list_of_dict:
+        if not i["char"].isalpha():
+            continue
+        else:
+            print(f"{i['char']}: {i['num']} ")
